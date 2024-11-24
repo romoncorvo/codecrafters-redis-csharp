1d781fa6ac121e467a1e5bcd3c0b74599e13ada1
Rodrigo <romoncorvo@gmail.com>
2024-11-24 14:02:59 +0100

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -8,22 +8,43 @@ internal static class Program
 {
     public static async Task Main(string[] args)
     {
-        var listener  = new TcpListener(IPAddress.Any, 6379);
+        IPEndPoint ipEndPoint = new(IPAddress.Any, 6379);
+        
+        using Socket listener = new(
+            ipEndPoint.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+        
+        const string eom = "<|EOM|>";
         
         try
         {
-            listener.Start();
-
-            using var handler = await listener.AcceptTcpClientAsync();
-            await using var stream = handler.GetStream();
-
-            var message = "+PONG\r\n";
-            var dateTimeBytes = Encoding.UTF8.GetBytes(message);
-            await stream.WriteAsync(dateTimeBytes);
+            listener.Bind(ipEndPoint);
+            listener.Listen(100);
+            var handler = await listener.AcceptAsync();
+            
+            while (true)
+            {
+                var buffer = new byte[1_024];
+                var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                
+                if (response.Contains("PING", StringComparison.Ordinal))
+                {
+                    Console.WriteLine(
+                        $"Socket server received message: \"{response}\"");
+                    
+                    var pong = "+PONG\r\n";
+                    var echoBytes = Encoding.UTF8.GetBytes(pong);
+                    await handler.SendAsync(echoBytes, 0);
+                    Console.WriteLine(
+                        $"Socket server sent acknowledgment: \"{pong}\"");
+                }
+            }
         }
         finally
         {
-            listener.Stop();
+            listener.Close();
         }
     }
 }
